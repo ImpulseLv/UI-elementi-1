01480cc1fe84856797eab7bc76d6410f987daf62
Ternoviks <ternovik.daniil@gmail.com>
2022-02-22 14:00:21 +0200

    Lielaka dala no projekta ir pabeigta

--- Assets/Skripti/AtteluParadisana.cs
+++ Assets/Skripti/AtteluParadisana.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AtteluParadisana : MonoBehaviour {
 	public GameObject bins;
 	public GameObject lacitis;
 	public GameObject tante;
 	public GameObject masina;
+	public GameObject attelaTuretajs;
+	public GameObject slaideris;
+	public Sprite[] atelluMasivs;
+
 
 	public void binaAttelosana(bool vertiba){
 		bins.SetActive (vertiba);
@@ -30,5 +35,18 @@ public class AtteluParadisana : MonoBehaviour {
 
 	public void paLabiBins(){
 		bins.transform.localScale = new Vector2 (-1, 1);
+	}
+	public void izkritosais (int skaitlis){
+		if (skaitlis == 0)
+			attelaTuretajs.GetComponent<Image> ().sprite = atelluMasivs [0];
+		else if (skaitlis == 1)
+			attelaTuretajs.GetComponent<Image> ().sprite = atelluMasivs [1];
+		else if (skaitlis == 2)
+			attelaTuretajs.GetComponent<Image> ().sprite = atelluMasivs [2];
+	}
+
+	public void mainitLielumu (){
+		float pasreizejaVertiba = slaideris.GetComponent<Slider> ().value;
+		attelaTuretajs.transform.localScale = new Vector2 (1F * pasreizejaVertiba, 1F * pasreizejaVertiba);
 	}
 }
